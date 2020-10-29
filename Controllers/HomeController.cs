8b2d7f2886041cb65f8e93d3ae94c06e9c28f51d
Hamza <hamza40nazir@gmail.com>
2020-10-29 08:54:25 +0500

    mvc tutorial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Foo()
+        {
+            return View("Privacy");
+        }
+
+        public ActionResult Serial(string letterCase)
+        {
+            var serial = "ABCDEF";
+            if(letterCase =="lower")
+            {
+                return Content(serial.ToLower());
+            }
+            return Content(serial.ToLower());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
